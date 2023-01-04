9da27eb893c5366c03f329bf693e1ec96bab9bec
민경박 <tofhddl94@gmail.com>
2023-01-04 18:57:09 +0900

    Marcket

--- src/market/run/Application.java
+++ src/market/run/Application.java
@@ -6,24 +6,46 @@ public class Application {
 
 	public static void main(String[] args) {
 
-//		Scanner sc = new Scanner(System.in);
-//		System.out.print("이름을 입력하세요 : ");
-//		String name = sc.next();
-//
-//		System.out.println("==================");
-//		System.out.println("오늘의 장보기 : ");
-		
 		Himedia hi = new Himedia();
 		Product pro = new Product();
 		Izen izen = new Izen();
 		Sangyong sangyong = new Sangyong();
-		
-		hi.selectProduct(pro.loadProductList());
-		izen.selectProduct(pro.loadProductList());
-		sangyong.selectProduct(pro.loadProductList());
-		
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("이름을 입력하세요 : ");
+		String name = sc.next();
+
+		System.out.println("==================");
+		System.out.println("오늘의 장보기 : ");
+
+		while (true) {
+			System.out.println("마트를 선택해주세요.");
+			System.out.println("A 마트 : 식품 10프로 할인");
+			System.out.println("B 마트 : 정육 10프로 할인");
+			System.out.println("C 마트 : 생활용품 할인 10프로 할인");
+
+			char select = sc.next().charAt(0);
+
+			if (select == '1') {
+				hi.selectProduct(pro.loadProductList());
+				break;
+			} else if (select == '2') {
+				izen.selectProduct(pro.loadProductList());
+				break;
+			} else if (select == '3') {
+				sangyong.selectProduct(pro.loadProductList());
+				break;
+			} else {
+				System.out.println("존재하는 마트를 선택해주세요.");
+				continue;
+			}
+
+		}
+
+		// 받아온 배열중에서 랜덤으로 배열인덱스, 상품 리스트 3개를 추출해서 >마트를 고르고 A마트 / B 마트 / C 마트 > 그 마트에서 고른
+		// 상품의 가격을 다 더하고 내가 가지고 있는 돈에서 뺀다
+		// > 프로덕트에 영수증을 출력하는 메소드를 짜서 오버라이드 > 아예 다 끝나고 엄마찬스를 돌려서 남은 돈에 선택한 금액만큼
+		// 추가 해주고 > 그게 0보다 크면 성공! 0보다 작으면 실패!
 
 	}
-
-	
 }
--- src/market/run/Himedia.java
+++ src/market/run/Himedia.java
@@ -4,33 +4,11 @@ import java.util.Arrays;
 
 public class Himedia extends Product {
 
-//	public void selectProduct(Product[] prod) {
-//		int money = (int) (Math.random() * 50 + 1) * 1000;
-//
-//		Product[] product = prod;
-//
-//		int result = 0;
-//		for (int i = 0; i < product.length; i++) {
-//			switch (product[i].getIndex()) {
-//			case 1:
-//				result = product[i].getPrice();
-//				double discount = (result * 0.9);
-//
-//				System.out.println(discount);
-//
-//				double result2 = money - discount;
-//
-//				System.out.println(result2);
-//			default:
-//				break;
-//			}
-//		}
-//
-//	}
-
 	@Override
 	public void selectProduct(Product[] prod) {
 		int money = (int) (Math.random() * 50 + 1) * 1000;
+		
+		System.out.println(money);
 
 		Product[] product = prod;
 
@@ -40,11 +18,12 @@ public class Himedia extends Product {
 			case 1:
 				result = product[i].getPrice();
 				double discount = (result * 0.9);
-
-				System.out.println(discount);
+				
+				System.out.println("price : " +result);
+				System.out.println("할인된 값 : " + discount);
 				
 				double result2 = money - discount;
-				System.out.println(result2);
+				System.out.println("남은 돈 : " + result2);
 
 			default:
 				break;
